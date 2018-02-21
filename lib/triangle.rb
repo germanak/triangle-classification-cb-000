a9cf4aa96c1237632b3c8727473c8abc65a0fe77
Alec German <alec_german@yahoo.com>
2018-02-21 05:11:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,11 @@
 class Triangle
   # write code here
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :a, :b, :c 
 
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-  end
-
-  def kind
-    if self.side1 < 0 && self.side2 < 0 && self.side3 < 0
-      raise TriangleError
-    else
-      if
-    end
+  def initialize(a, b, c)
+    @a = a
+    @b = b 
+    @c = c 
   end
 end
 
