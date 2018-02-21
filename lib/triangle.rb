3ce978923aa25212aff1e40961c3a5315e473170
Alec German <alec_german@yahoo.com>
2018-02-21 05:11:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,15 @@
 class Triangle
   # write code here
-  attr_accessor :a, :b, :c 
+  attr_accessor :a, :b, :c
 
   def initialize(a, b, c)
     @a = a
-    @b = b 
-    @c = c 
+    @b = b
+    @c = c
+  end
+
+  def kind
+
   end
 end
 
